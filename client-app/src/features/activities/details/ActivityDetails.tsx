2a47db96d8659e288620a2176a0f55f3a36b127f
Wim Van Der Schueren <wim.vanderschueren@allphi.eu>
2019-11-15 12:20:35 +0100

    React: CRUD Activities (No calls to API)

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { Card, Image, Icon, Button } from "semantic-ui-react";
+import { Card, Image, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 
 interface IProps {
   activity: IActivity;
+  setEditMode: (editMode: boolean) => void;
+  setSelectedActivity: (activity: IActivity | null) => void;
 }
 
-const ActivityDetails: React.FC<IProps> = ({ activity }) => {
+const ActivityDetails: React.FC<IProps> = ({
+  activity,
+  setEditMode,
+  setSelectedActivity
+}) => {
   return (
     <Card fluid>
       <Image
@@ -23,8 +29,18 @@ const ActivityDetails: React.FC<IProps> = ({ activity }) => {
       </Card.Content>
       <Card.Content extra>
         <Button.Group widths={2}>
-          <Button basic color="blue" content="Edit" />
-          <Button basic color="grey" content="Cancel" />
+          <Button
+            onClick={() => setEditMode(true)}
+            basic
+            color="blue"
+            content="Edit"
+          />
+          <Button
+            onClick={() => setSelectedActivity(null)}
+            basic
+            color="grey"
+            content="Cancel"
+          />
         </Button.Group>
       </Card.Content>
     </Card>
